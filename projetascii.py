86b11a03b5b91d7f566c271fbf45b488a4ce225f
antoine <antoineccmm@msn.com>
2015-10-02 16:08:51 -0400

    premier commit

---
+++ projetascii.py
@@ -0,0 +1,115 @@
+#Votre méthode doit comprendre ceci :
+#input("votre nom")
+#votre code
+#input("un message d'aurevoir")
+#self.clear() sert à vider lecran pour le prochain projet
+
+import os
+class projetASCIIART:
+	def __init__(self):
+		self.clear = lambda: os.system('cls')
+
+	def asciiprof(self):
+		input("Projet du prof")
+		print ("Mon nom est Antoine et voici le travail de mes étudiants")
+		print ( "     ,.----------.     ")
+		print ( "    ((|          |     ")
+		print ( "   .--\\          /--. ")
+		print ( "  '._  '========'  _.' ")
+		print ( '     `""""""""""""`    ')
+		print ( "Le cafe est le meilleur ami de l'informaticien !")
+		input("Fin du projet du prof")
+		self.clear()
+	def ascii1(self):
+		pass
+	def ascii2(self):
+		pass
+	def ascii3(self):
+		pass
+	def ascii4(self):
+		pass
+	def ascii5(self):
+		pass
+	def ascii6(self):
+		pass
+	def ascii7(self):
+		pass
+	def ascii8(self):
+		pass
+	def ascii9(self):
+		pass
+	def ascii10(self):
+		pass
+	def ascii11(self):
+		pass
+	def ascii12(self):
+		pass
+	def ascii13(self):
+		pass
+	def ascii14(self):
+		pass
+	def ascii15(self):
+		pass
+	def ascii16(self):
+		pass
+	def ascii17(self):
+		pass
+	def ascii18(self):
+		pass
+	def ascii19(self):
+		pass
+	def ascii20(self):
+		pass
+	def ascii21(self):
+		pass
+	def ascii22(self):
+		pass
+	def ascii23(self):
+		pass
+	def ascii24(self):
+		pass
+	def ascii25(self):
+		pass
+	def ascii26(self):
+		pass
+	def ascii27(self):
+		pass
+	def ascii28(self):
+		pass
+	def ascii29(self):
+		pass
+	def ascii30(self):
+		pass
+if __name__ == "__main__":
+	p = projetASCIIART()
+	p.asciiprof()
+	p.ascii1()
+	p.ascii2()
+	p.ascii3()
+	p.ascii4()
+	p.ascii5()
+	p.ascii6()
+	p.ascii7()
+	p.ascii8()
+	p.ascii9()
+	p.ascii10()
+	p.ascii11()
+	p.ascii12()
+	p.ascii13()
+	p.ascii14()
+	p.ascii15()
+	p.ascii16()
+	p.ascii17()
+	p.ascii18()
+	p.ascii19()
+	p.ascii20()
+	p.ascii21()
+	p.ascii22()
+	p.ascii23()
+	p.ascii24()
+	p.ascii25()
+	p.ascii26()
+	p.ascii27()
+	p.ascii28()
+	p.ascii29()
+	p.ascii30()
